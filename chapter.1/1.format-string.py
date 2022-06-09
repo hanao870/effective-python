4dbfd6fe4bf25d62d3d4fdabca04b998a51960b4
hanao870 <hanackholehole@gmail.com>
2022-06-09 20:38:32 +0900

    [update] fotmat string の出力と同じフォーマット記述追加
    C スタイルフォーマット、str.format、辞書フォーマット

--- chapter.1/1.format-string.py
+++ chapter.1/1.format-string.py
@@ -13,3 +13,13 @@ if __name__ == "__main__":
     # .2f 小数点第3位を四捨五入
     formatted = f"{key!r:<10} = {value:.2f}"
     print(formatted)
+
+    # 以下のフォーマット結果は全て同じ
+    f_string = f"{key:<10} = {value:.2f}"
+    c_tuple = "%-10s = %.2f" % (key, value)
+    str_args = "{:<10} = {:.2f}".format(key, value)
+    str_kw = "{key:<10} = {value:.2f}".format(key=key, value=value)
+    c_dict = "%(key)-10s = %(value).2f" % {"key": key, "value": value}
+
+    assert c_tuple == c_dict == f_string
+    assert str_args == str_kw == f_string
