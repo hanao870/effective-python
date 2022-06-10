37b4e57fb1adf31cfdbc00cfbc4c30b332e32331
hanao870 <hanackholehole@gmail.com>
2022-06-10 20:56:39 +0900

    [update] 項目6 動作確認モジュール
    複雑なアンパック

--- chapter.1/6.unpack.py
+++ chapter.1/6.unpack.py
@@ -21,3 +21,21 @@ if __name__ == "__main__":
     first, second = pair
     print(f"{first} and {second}")
     print("-" * 50)
+
+    # 複雑なアンパック
+    favorite_snacks = {
+        "salty": ("pretzels", 100),
+        "sweet": ("cookie", 180),
+        "veggie": ("carrots", 20),
+    }
+
+    (
+        (type1, (name1, cals1)),
+        (type2, (name2, cals2)),
+        (type3, (name3, cals3)),
+    ) = favorite_snacks.items()
+
+    print(f"Favorite {type1} is {name1} with {cals1} calories")
+    print(f"Favorite {type2} is {name2} with {cals2} calories")
+    print(f"Favorite {type3} is {name3} with {cals3} calories")
+    print("-" * 50)
