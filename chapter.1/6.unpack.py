856de22a4996e8693b12a07eb1250469b516b6da
hanao870 <hanackholehole@gmail.com>
2022-06-10 20:12:49 +0900

    [update] 項目6 動作確認モジュール
    タプルの値のインデックスアクセス

--- chapter.1/6.unpack.py
+++ chapter.1/6.unpack.py
@@ -4,3 +4,9 @@ if __name__ == "__main__":
     snack_calories = {"chips": 140, "popcorn": 80, "nuts": 190}
     items = tuple(snack_calories.items())
     print(items)
+
+    # タプルの値をインデックスでアクセス
+    item = ("Peanut butter", "Jelly")
+    first = item[0]
+    second = item[1]
+    print(f"{first} and {second}")
