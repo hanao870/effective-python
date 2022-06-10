485af9bf158429caa187f4d176170f427a19fc38
hanao870 <hanackholehole@gmail.com>
2022-06-10 20:24:14 +0900

    [update] 項目6 動作確認モジュール
    アンパックによる値の取得

--- chapter.1/6.unpack.py
+++ chapter.1/6.unpack.py
@@ -4,13 +4,20 @@ if __name__ == "__main__":
     snack_calories = {"chips": 140, "popcorn": 80, "nuts": 190}
     items = tuple(snack_calories.items())
     print(items)
+    print("-" * 50)
 
     # タプルの値をインデックスでアクセス
     item = ("Peanut butter", "Jelly")
     first = item[0]
     second = item[1]
     print(f"{first} and {second}")
+    print("-" * 50)
 
     # タプルに代入(エラーとなる)
     pair = ("Chocolate", "Peanut butter")
     # pair[0] = "Honey"
+
+    # アンパックによる値の取得
+    first, second = pair
+    print(f"{first} and {second}")
+    print("-" * 50)
