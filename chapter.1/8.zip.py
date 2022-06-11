892a25fb82b68a5ec92c3ffda7e22fb703fca28b
hanao870 <hanackholehole@gmail.com>
2022-06-11 20:30:11 +0900

    [update] 項目8 動作確認モジュール
    リストの最大文字数の要素を取得する with zip_longets

--- chapter.1/8.zip.py
+++ chapter.1/8.zip.py
@@ -1,4 +1,5 @@
 """項目8:イテレータを並列に処理するには zip を使う."""
+import itertools
 from typing import Optional
 
 
@@ -34,6 +35,21 @@ def _using_zip(data: list[str]) -> None:
     print("-" * 50)
 
 
+def _zip_diff_len(data: list[str]) -> None:
+    counts = [len(n) for n in data]
+    data.append("Rosalind")
+
+    print(f"{len(data)=}, {len(counts)=}")
+    for name, count in zip(data, counts):
+        print(f"{name=}, {count=}")
+    print("-" * 50)
+
+    # 要素数の多いリストに合わせた zip 処理
+    for name, count in itertools.zip_longest(data, counts):
+        print(f"{name}: {count}")
+    print("-" * 50)
+
+
 if __name__ == "__main__":
     # リスト内包表記による文字数カウント
     names = ["Cecilia", "Lise", "Marie"]
@@ -56,3 +72,4 @@ if __name__ == "__main__":
 
     _usnig_enumerate(names)
     _using_zip(names)
+    _zip_diff_len(names)
