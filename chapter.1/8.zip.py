97892f4fc9ce6aa80a55e230a061c9e9fceaf150
hanao870 <hanackholehole@gmail.com>
2022-06-11 20:09:47 +0900

    [update] 項目8 動作確認モジュール
    リストの最大文字数の要素を取得する with enumerate

--- chapter.1/8.zip.py
+++ chapter.1/8.zip.py
@@ -1,6 +1,20 @@
 """項目8:イテレータを並列に処理するには zip を使う."""
-
 from typing import Optional
+
+
+def _usnig_enumerate(data: list[str]) -> None:
+    longest_name: Optional[str] = None
+    max_count = 0
+
+    for i, name in enumerate(data):
+        count = len(data[i])
+        if count > max_count:
+            longest_name = data[i]
+            max_count = count
+
+    print(f"{longest_name=}, {max_count=}")
+    print("-" * 50)
+
 
 if __name__ == "__main__":
     # リスト内包表記による文字数カウント
@@ -19,5 +33,7 @@ if __name__ == "__main__":
             longest_name = names[i]
             max_count = count
 
-    print(f"{max_count=}, {longest_name=}")
+    print(f"{longest_name=}, {max_count=}")
     print("-" * 50)
+
+    _usnig_enumerate(names)
