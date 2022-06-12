a7f647d4d4ebef763cf1cede0f232f8b16133fc3
hanao870 <hanackholehole@gmail.com>
2022-06-12 20:54:30 +0900

    [update] 項目10 動作確認モジュール
    代入式の値判定

--- chapter.1/10.walrus.py
+++ chapter.1/10.walrus.py
@@ -12,6 +12,11 @@ def _out_of_stock() -> None:
     pass
 
 
+@func_name
+def _make_cider(count: int) -> None:
+    pass
+
+
 if __name__ == "__main__":
     fresh_fruit = {"apple": 10, "banana": 8, "lemon": 5}
 
@@ -24,5 +29,18 @@ if __name__ == "__main__":
     # 上記の if を代入式で行う
     if count := fresh_fruit.get("lemon", 0):
         _make_lemonade(count)
+    else:
+        _out_of_stock()
+
+    # リンゴの個数判定
+    count = fresh_fruit.get("apple", 0)
+    if count >= 4:
+        _make_cider(count)
+    else:
+        _out_of_stock()
+
+    # 上記 if を代入式で行う
+    if (count := fresh_fruit.get("apple", 0)) >= 4:
+        _make_cider(count)
     else:
         _out_of_stock()
