6a983e57629ebdae9330fe2438e9cccddcce3173
hanao870 <hanackholehole@gmail.com>
2022-06-12 21:13:00 +0900

    [update] 項目11 動作確認モジュール
    先頭/末尾を含むスライス.
    start / end の指定は不要

--- chapter.1/11.sequence_slice.py
+++ chapter.1/11.sequence_slice.py
@@ -3,7 +3,15 @@
 
 if __name__ == "__main__":
     a = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+    print(f"{a=}")
+
     # list[start:end] でスライス.
     # start は含まれ、end は含まれない
     print(f"Middle two:     {a[3:5]}")
     print(f"All but ends:   {a[1:7]}")
+
+    # 先頭からのスライス a[0:5] と同じ
+    print(f"{a[:5]=}")
+    # 末尾までのスライス a[5:len(a)] と同じ
+    print(f"{a[5:]=}")
