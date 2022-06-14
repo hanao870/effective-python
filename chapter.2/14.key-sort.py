9ec8f2fc82c49c9f4cd7974f0f625e683f682146
hanao870 <hanackholehole@gmail.com>
2022-06-14 18:23:37 +0900

    [add] 項目14 動作確認モジュール
    リストのソート

---
+++ chapter.2/14.key-sort.py
@@ -0,0 +1,7 @@
+"""項目14:key 引数を使い複雑な基準でソートする."""
+if __name__ == "__main__":
+    numbers = [93, 86, 11, 68, 70]
+    print(f"Sort Before: {numbers}")
+
+    numbers.sort()
+    print(f"Sort After : {numbers}")
