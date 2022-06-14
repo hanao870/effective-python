e09932afd0e1a9b001a3d062388a06695e44ef5c
hanao870 <hanackholehole@gmail.com>
2022-06-14 20:14:52 +0900

    [add] 項目15 動作確認モジュール
    キーワード引数の print 表示.
    渡した順に表示される.

--- chapter.2/15.dict-insert.py
+++ chapter.2/15.dict-insert.py
@@ -1,4 +1,12 @@
 """項目15:dict の挿入順序に依存する場合は注意する."""
+
+
+def _my_func(**kwargs: str) -> None:
+    for key, value in kwargs.items():
+        print(f"{key} = {value}")
+    print("-" * 50)
+
+
 if __name__ == "__main__":
     baby_name = {"cat": "kitten", "dog": "puppy"}
     # Python 3.5 以前は、辞書への挿入順ではなく Key でソートされる
@@ -11,3 +19,5 @@ if __name__ == "__main__":
     # 最終挿入要素
     print(baby_name.popitem())
     print("-" * 50)
+
+    _my_func(goose="gosling", kangaroo="joey")
