638f9a5163e2f4072de0583c3f9f90a8f418123c
hanao870 <hanackholehole@gmail.com>
2022-06-14 20:56:12 +0900

    [add] 項目15 動作確認モジュール
    動物の赤ちゃんコンテストの結果表示.
    独自辞書に結果を格納する方法を記述.
    mypy の型アノテーションでエラーとなる.

--- chapter.2/15.dict-insert.py
+++ chapter.2/15.dict-insert.py
@@ -1,4 +1,61 @@
 """項目15:dict の挿入順序に依存する場合は注意する."""
+from collections.abc import MutableMapping
+from typing import Iterator
+
+
+class SortedDict(MutableMapping[str, int]):
+    """..."""
+
+    def __init__(self) -> None:
+        """イニシャライザ."""
+        self.data: dict[str, int] = {}
+
+    def __getitem__(self, key: str) -> int:
+        """`key` の値を取得する.
+
+        Args:
+            key (str): 値を取得するキー
+
+        Returns:
+            int: `key` の値
+        """
+        return self.data[key]
+
+    def __setitem__(self, key: str, value: int) -> None:
+        """`key` に対応する `value` を設定する.
+
+        Args:
+            key (str):  キー
+            value (int): `key` の値
+        """
+        self.data[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        """`key` を削除する.
+
+        Args:
+            key (str): 削除するキー
+        """
+        del self.data[key]
+
+    def __iter__(self) -> Iterator[str]:
+        """クラス内辞書のイテレータを返す.
+
+        Yields:
+            Iterator[str]: 辞書のイテレータ
+        """
+        keys = list(self.data.keys())
+        keys.sort()
+        for key in keys:
+            yield key
+
+    def __len__(self) -> int:
+        """辞書の長さを返す.
+
+        Returns:
+            int: クラス内辞書の長さ
+        """
+        return len(self.data)
 
 
 class MyClass:
@@ -56,3 +113,11 @@ if __name__ == "__main__":
     winner = _get_winner(ranks)
     print(winner)
     print("-" * 50)
+
+    # 標準辞書ではなく SortedDict に結果を格納する
+    # mypy で型エラーが発生. 実行はできるが、結果は間違っている('fox' と表示される)
+    # sorted_ranks = SortedDict()
+    # _popular_ranks(votes, sorted_ranks)
+    # print(sorted_ranks.data)
+    # winner = _get_winner(sorted_ranks)
+    # print(winner)
