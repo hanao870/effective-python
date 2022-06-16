2a37575c17802a0cf90b0e292f9d0978fd26bfb4
hanao870 <hanackholehole@gmail.com>
2022-06-16 18:49:01 +0900

    [update] 項目17 動作確認モジュール
    訪問国と都市を管理するクラス追加.

--- chapter.2/17.defaultdict.py
+++ chapter.2/17.defaultdict.py
@@ -1,4 +1,27 @@
 """項目17:内部状態の欠損要素を扱うには setdefault ではなく defaultdict を使う."""
+
+
+class Visits:
+    """訪問した国と都市を管理するクラス."""
+
+    def __init__(self) -> None:
+        """イニシャライザ.
+
+        辞書を初期化
+        """
+        self.data: dict[str, set[str]] = {}
+
+    def add(self, country: str, city: str) -> None:
+        """`country` に `city` を追加する.
+
+        Args:
+            country (str): 訪問国
+            city (str): 都市名
+        """
+        city_set = self.data.setdefault(country, set())
+        city_set.add(city)
+
+
 if __name__ == "__main__":
     visits = {"Mexico": {"Tulum", "Puerto Vallarta"}, "Japan": {"Hakone"}}
 
