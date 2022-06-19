5ec034152c325897b0cfe5aa3b777afcd5203667
hanao870 <hanackholehole@gmail.com>
2022-06-19 20:46:34 +0900

    [update] 項目19 動作確認モジュール
    戻り値が5つの関数のアンパック(見にくい).

--- chapter.3/19.func-unpack.py
+++ chapter.3/19.func-unpack.py
@@ -22,6 +22,25 @@ def _get_avg_ration(numbers: list[int]) -> list[float]:
     return scaled
 
 
+@func_name
+def _get_stats_new(numbers: list[int]) -> tuple[int, int, float, float, int]:
+    minimum = min(numbers)
+    maximum = max(numbers)
+    count = len(numbers)
+    average = sum(numbers) / count
+
+    sorted_numbers = sorted(numbers)
+    middle = count // 2
+    if count % 2 == 0:
+        lower = sorted_numbers[middle - 1]
+        upper = sorted_numbers[middle]
+        median = (lower + upper) / 2
+    else:
+        median = sorted_numbers[middle]
+
+    return minimum, maximum, average, median, count
+
+
 if __name__ == "__main__":
     lengths = [63, 73, 72, 60, 67, 66, 71, 61, 72, 70]
     minimum, maximum = _get_stats(lengths)
@@ -39,3 +58,8 @@ if __name__ == "__main__":
     print(f"Longest:  {longest:>4.0%}")
     print(f"Shortest: {shortest:>4.0%}")
     print(f"Middle:   {middle}")
+
+    # 複数戻り値のアンパック(多すぎ!!!)
+    minimum, maximum, average, median, count = _get_stats_new(lengths)
+    print(f"Min: {minimum}, Max: {maximum}")
+    print(f"Average: {average}, Median: {median}, Count: {count}")
