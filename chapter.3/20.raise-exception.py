a1f9c8be4883af0f7a1240087590e0a8076e0320
hanao870 <hanackholehole@gmail.com>
2022-06-19 21:14:35 +0900

    [update] 項目20 動作確認モジュール
    戻り値に成否判定追加.
    但し、戻り値は簡単に無視できる.

--- chapter.3/20.raise-exception.py
+++ chapter.3/20.raise-exception.py
@@ -12,6 +12,14 @@ def _careful_divide(a: int, b: int) -> Optional[float]:
         return None
 
 
+@func_name
+def _careful_divide_tuple(a: int, b: int) -> tuple[bool, Optional[float]]:
+    try:
+        return True, a / b
+    except ZeroDivisionError:
+        return False, None
+
+
 if __name__ == "__main__":
     x, y = 1, 0
     result = _careful_divide(x, y)
@@ -21,5 +29,15 @@ if __name__ == "__main__":
     x, y = 0, 5
     result = _careful_divide(x, y)
     # None と 0 の判定となる
+    if not result:
+        print("Invalid inputs")
+
+    # 第1戻り値に成否を受け取る
+    success, result = _careful_divide_tuple(1, 0)
+    if not success:
+        print("Invalid inputs")
+
+    # 但し、戻り値は簡単に無視できる. result が 0 の場合に問題あり
+    _, result = _careful_divide_tuple(0, 5)
     if not result:
         print("Invalid inputs")
