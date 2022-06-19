ae43d45e0400324f882e33f7862d1185cec54200
hanao870 <hanackholehole@gmail.com>
2022-06-19 20:27:58 +0900

    [update] 項目19 動作確認モジュール
    関数戻り値のアンパック動作確認

--- chapter.3/19.func-unpack.py
+++ chapter.3/19.func-unpack.py
@@ -9,7 +9,19 @@ def _get_stats(numbers: list[int]) -> tuple[int, int]:
     return minimum, maximum
 
 
+@func_name
+def _my_function() -> tuple[int, int]:
+    return 3, 4
+
+
 if __name__ == "__main__":
     lengths = [63, 73, 72, 60, 67, 66, 71, 61, 72, 70]
     minimum, maximum = _get_stats(lengths)
     print(f"Min: {minimum}, Max: {maximum}")
+
+    # アンパックと関数戻り値の動作確認
+    first, second = 1, 2
+    print(f"{first=}, {second=}")
+
+    first, second = _my_function()
+    print(f"{first=}, {second=}")
