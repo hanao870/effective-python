50533bdae3ee5680039dbc50d1558c9de432cf1a
hanao870 <hanackholehole@gmail.com>
2022-06-19 21:25:23 +0900

    [update] 項目20 動作確認モジュール
    例外の送出と型アノテーション + docstring の組み合わせ

--- chapter.3/20.raise-exception.py
+++ chapter.3/20.raise-exception.py
@@ -20,6 +20,34 @@ def _careful_divide_tuple(a: int, b: int) -> tuple[bool, Optional[float]]:
         return False, None
 
 
+@func_name
+def _careful_divide_raise(a: int, b: int) -> float:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ValueError("Invalid inputs")
+
+
+@func_name
+def careful_divide(a: float, b: float) -> float:
+    """Divides a by b.
+
+    Args:
+        a (float): 分母
+        b (float): 分子
+
+    Returns:
+        float: a / b の値
+
+    Raises:
+        ValueError: a / b が失敗
+    """
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ValueError("Invalid inputs")
+
+
 if __name__ == "__main__":
     x, y = 1, 0
     result = _careful_divide(x, y)
@@ -41,3 +69,18 @@ if __name__ == "__main__":
     _, result = _careful_divide_tuple(0, 5)
     if not result:
         print("Invalid inputs")
+
+    x, y = 5, 2
+    try:
+        result = _careful_divide_raise(x, y)
+    except ValueError as err:
+        print(err)
+    else:
+        print(f"Result is {result:.1f}")
+
+    try:
+        result = careful_divide(3, 0)
+    except ValueError as err:
+        print(err)
+    else:
+        print(f"Result is {result:.1f}")
