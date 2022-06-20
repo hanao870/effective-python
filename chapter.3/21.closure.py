ad35907cda6a48459b13fcc19a31f68286450186
hanao870 <hanackholehole@gmail.com>
2022-06-20 21:29:19 +0900

    [update] 項目21 動作確認モジュール
    ソートの key となるへルパークラス追加.

--- chapter.3/21.closure.py
+++ chapter.3/21.closure.py
@@ -42,6 +42,33 @@ def _sort_priority3(numbers: list[int], group: set[int]) -> bool:
     return found
 
 
+class Sorter:
+    """ソートのヘルパークラス."""
+
+    def __init__(self, group: set[int]) -> None:
+        """イニシャライザ.
+
+        Args:
+            group (set[int]): ソートで優先する値のグループ
+        """
+        self.group = group
+        self.found = False
+
+    def __call__(self, x: int) -> tuple[int, int]:
+        """...
+
+        Args:
+            x (int): ソート対象の値
+
+        Returns:
+            tuple[int, int]: ソート対象のインデックス
+        """
+        if x in self.group:
+            self.found = True
+            return (0, x)
+        return (1, x)
+
+
 if __name__ == "__main__":
     numbers = [8, 3, 1, 2, 5, 4, 7, 6]
     group = {2, 3, 5, 7}
@@ -53,5 +80,10 @@ if __name__ == "__main__":
     print(f"{numbers=}")
 
     found = _sort_priority3(numbers, group)
+    print(f"Found: {found}")
+    print(f"{numbers=}")
+
+    sorter = Sorter(group)
+    numbers.sort(key=sorter)
     print(f"Found: {found}")
     print(f"{numbers=}")
