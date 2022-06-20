0d4a67edf2722926928baf3cba272328a98c705d
hanao870 <hanackholehole@gmail.com>
2022-06-20 21:16:30 +0900

    [update] 項目21 動作確認モジュール
    クロージャ関数で変数の代入.
    新たな変数定義となる.

--- chapter.3/21.closure.py
+++ chapter.3/21.closure.py
@@ -12,8 +12,27 @@ def _sort_priority(values: list[int], group: set[int]) -> None:
     values.sort(key=_helper)
 
 
+@func_name
+def _sort_priority2(numbers: list[int], group: set[int]) -> bool:
+    found = False
+
+    def _helper(x: int) -> tuple[int, int]:
+        if x in group:
+            # クロージャ関数での代入は新たな変数定義となる
+            # found = True  # 簡単そうに見える
+            return (0, x)
+        return (1, x)
+
+    numbers.sort(key=_helper)
+    return found
+
+
 if __name__ == "__main__":
     numbers = [8, 3, 1, 2, 5, 4, 7, 6]
     group = {2, 3, 5, 7}
     _sort_priority(numbers, group)
+    print(f"{numbers=}")
+
+    found = _sort_priority2(numbers, group)
+    print(f"Found: {found}")
     print(f"{numbers=}")
