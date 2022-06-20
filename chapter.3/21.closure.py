bb3969db35ff965e0c3f1c49b3ec9695f6dc0f90
hanao870 <hanackholehole@gmail.com>
2022-06-20 21:20:54 +0900

    [update] 項目21 動作確認モジュール
    クロージャ関数外へスコープを横断する nonlocal.

--- chapter.3/21.closure.py
+++ chapter.3/21.closure.py
@@ -27,6 +27,21 @@ def _sort_priority2(numbers: list[int], group: set[int]) -> bool:
     return found
 
 
+@func_name
+def _sort_priority3(numbers: list[int], group: set[int]) -> bool:
+    found = False
+
+    def _helper(x: int) -> tuple[int, int]:
+        nonlocal found  # スコープの横断
+        if x in group:
+            found = True
+            return (0, x)
+        return (1, x)
+
+    numbers.sort(key=_helper)
+    return found
+
+
 if __name__ == "__main__":
     numbers = [8, 3, 1, 2, 5, 4, 7, 6]
     group = {2, 3, 5, 7}
@@ -34,5 +49,9 @@ if __name__ == "__main__":
     print(f"{numbers=}")
 
     found = _sort_priority2(numbers, group)
+    print(f"Found: {found}")
+    print(f"{numbers=}")
+
+    found = _sort_priority3(numbers, group)
     print(f"Found: {found}")
     print(f"{numbers=}")
