995d88a37580410cb95aae1d7bf63662d423bd07
hanao870 <hanackholehole@gmail.com>
2022-06-21 18:49:33 +0900

    [update] 項目23 動作確認モジュール
    キーワード引数に辞書を渡す.
    重複がない限り ** で複数渡せる.

--- chapter.3/23.keyword-args.py
+++ chapter.3/23.keyword-args.py
@@ -19,3 +19,16 @@ if __name__ == "__main__":
     # print(_remainder(number=20, 7))
     # 各引数は1回だけ指定できる
     # print(_remainder(20, number=7))
+
+    # キーワード引数に対応した辞書を渡す
+    my_kwargs = {"number": 20, "divisor": 7}
+    print(f"{_remainder(**my_kwargs)=}")
+
+    # キーワードに重複がない限り ** を渡せる
+    my_kwargs = {"divisor": 7}
+    print(f"{_remainder(number=20, **my_kwargs)=}")
+
+    # 重複がない限り複数の ** を渡せる
+    my_kwargs = {"number": 20}
+    other_kwargs = {"divisor": 7}
+    print(f"{_remainder(**my_kwargs, **other_kwargs)=}")
