252fbc0122b5a434f34040f038894d9f691bae56
hanao870 <hanackholehole@gmail.com>
2022-06-21 18:24:51 +0900

    [update] 項目22 動作確認モジュール
    可変長位置引数にジェネレータを渡す.
    全要素が tuple 展開されるのでメモリ消費に注意.

--- chapter.3/22.star-args.py
+++ chapter.3/22.star-args.py
@@ -1,4 +1,6 @@
 """項目22:可変長位置引数を使って、見た目をすっきりさせる."""
+from typing import Iterator
+
 from my_libs.decolator import func_name
 
 
@@ -20,6 +22,15 @@ def _log_1(message: str, *values: int) -> None:
         print(f"{message}: {value_str}")
 
 
+def _my_generator() -> Iterator[int]:
+    for i in range(10):
+        yield i
+
+
+def _my_func(*args: int) -> None:
+    print(args)
+
+
 if __name__ == "__main__":
     _log("My numbers are", [1, 2])
     _log("Hi there", [])
@@ -27,5 +38,11 @@ if __name__ == "__main__":
     _log_1("My numbers are", 1, 2)
     _log_1("Hi there")
 
+    # 可変長位置引数に配列を渡す
     favorites = [7, 33, 99]
     _log_1("Favorite colors", *favorites)
+
+    # 可変長位置引数は tuple に変換される.
+    # ジェネレータの全要素が展開されるため、メモリが大量消費される.
+    it = _my_generator()
+    _my_func(*it)
