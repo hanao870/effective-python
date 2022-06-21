0ea2239e897802a81fe76ee5ff13f747fab51e8f
hanao870 <hanackholehole@gmail.com>
2022-06-21 18:07:48 +0900

    [add] 項目22 動作確認モジュール
    可変長引数を使わない関数.

---
+++ chapter.3/22.star-args.py
@@ -0,0 +1,16 @@
+"""項目22:可変長位置引数を使って、見た目をすっきりさせる."""
+from my_libs.decolator import func_name
+
+
+@func_name
+def _log(message: str, values: list[int]) -> None:
+    if not values:
+        print(message)
+    else:
+        value_str = ", ".join(str(x) for x in values)
+        print(f"{message}: {value_str}")
+
+
+if __name__ == "__main__":
+    _log("My numbers are", [1, 2])
+    _log("Hi there", [])
