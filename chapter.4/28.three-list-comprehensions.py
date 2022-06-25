d06aea57fda5c246ee142aa625227df4593b8b87
hanao870 <hanackholehole@gmail.com>
2022-06-25 16:56:04 +0900

    [update] 項目28 動作確認モジュール
    リスト内包表記の3重ループ.
    for のループ構造のほうが可読性が良い.

--- chapter.4/28.three-list-comprehensions.py
+++ chapter.4/28.three-list-comprehensions.py
@@ -1,4 +1,4 @@
-"""項目28:内包表記では、3 つ以上のしきを避ける."""
+"""項目28:内包表記では、3 つ以上の式を避ける."""
 
 
 if __name__ == "__main__":
@@ -10,3 +10,17 @@ if __name__ == "__main__":
 
     squared = [[x**2 for x in row] for row in matrix]
     print(f"{squared=}")
+
+    my_lists = [[[1, 2], [3, 4]], [[5, 6], [7, 8], [9, 10]]]
+    print(f"{my_lists=}")
+
+    # リスト内包表記の3重ループ. 非常に読みにくい
+    my_flat = [x for sublist1 in my_lists for sublist2 in sublist1 for x in sublist2]
+    print(f"{my_flat=}")
+
+    # 上記をループ構造で表記. こちらのほうが読みやすい!
+    my_flat = []
+    for sublist1 in my_lists:
+        for sublist2 in sublist1:
+            my_flat.extend(sublist2)
+    print(f"{my_flat=}")
