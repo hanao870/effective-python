55c3811c5262a664a09513e2e44d9b4a42083e13
hanao870 <hanackholehole@gmail.com>
2022-06-25 16:47:41 +0900

    [update] 項目28 動作確認モジュール
    リスト内包表記の2重ループで2重リスト.

--- chapter.4/28.three-list-comprehensions.py
+++ chapter.4/28.three-list-comprehensions.py
@@ -3,5 +3,10 @@
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(f"{matrix=}")
+
     flat = [x for row in matrix for x in row]
-    print(flat)
+    print(f"{flat=}")
+
+    squared = [[x**2 for x in row] for row in matrix]
+    print(f"{squared=}")
