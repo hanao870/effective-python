fbd021f8b3ce260126e550d6ec39b654ecec8b7f
hanao870 <hanackholehole@gmail.com>
2022-06-25 20:05:12 +0900

    [add] 項目29 動作確認モジュール
    リスト内包表記での関数呼び出し..

---
+++ chapter.4/29.list-walrus-operator.py
@@ -0,0 +1,30 @@
+"""項目29:代入式を使い内包表記の繰り返し作業をなくす."""
+from my_libs.decolator import func_name
+
+stock = {"nails": 125, "screws": 35, "wingnuts": 8, "washers": 24}
+order = ["screws", "wingnuts", "clips"]
+
+
+@func_name
+def _get_bathes(count: int, size: int) -> int:
+    return count // size
+
+
+if __name__ == "__main__":
+    result: dict[str, int] = {}
+
+    for name in order:
+        count = stock.get(name, 0)
+        batches = _get_bathes(count, 8)
+        if batches:
+            result[name] = batches
+
+    print(f"{result=}")
+
+    # 上記ループと同じリスト内包表記
+    found = {
+        name: _get_bathes(stock.get(name, 0), 8)
+        for name in order
+        if _get_bathes(stock.get(name, 0), 8)
+    }
+    print(f"{found=}")
