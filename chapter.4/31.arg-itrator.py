a88a100c09d0faec5d9a1b36bfe8e1609f1ad779
hanao870 <hanackholehole@gmail.com>
2022-06-26 17:49:31 +0900

    [update] 項目31 動作確認モジュール
    イテレータを生成するラムダ関数を引数に渡す.

--- chapter.4/31.arg-itrator.py
+++ chapter.4/31.arg-itrator.py
@@ -1,6 +1,6 @@
 """項目31:引数に対してイテレータを使うときには確実さを優先する."""
 from pathlib import Path
-from typing import Iterator
+from typing import Callable, Iterator
 
 
 def _normalize(numbers: list[int]) -> list[float]:
@@ -32,10 +32,24 @@ def _normalize_copy(numbers_itr: Iterator[int]) -> list[float]:
     return result
 
 
+def _normalize_func(get_iter: Callable[[], Iterator[int]]) -> list[float]:
+    total = sum(get_iter())  # 新たなイテレータ
+    result = []
+
+    for value in get_iter():  # 新たなイテレータ
+        percent = 100 * value / total
+        result.append(percent)
+
+    return result
+
+
 if __name__ == "__main__":
+    N = 100
+
     visits = [15, 35, 80]
     percentages = _normalize(visits)
     print(f"{percentages=}, {sum(percentages)=}")
+    print("-" * N)
 
     file_path = Path(__file__).parent / "my_numbers.txt"
     it = _read_visits(str(file_path))
@@ -47,3 +61,9 @@ if __name__ == "__main__":
     it = _read_visits(str(file_path))
     percentages = _normalize_copy(it)
     print(f"{percentages=}, {sum(percentages)=}")
+    print("-" * N)
+
+    # イテレータを生成する関数を渡す
+    percentages = _normalize_func(lambda: _read_visits(str(file_path)))
+    print(f"{percentages=}, {sum(percentages)=}")
+    print("-" * N)
