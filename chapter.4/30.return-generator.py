0ae0c2af9b29fa3452c87a057ff869c00c54c0ed
hanao870 <hanackholehole@gmail.com>
2022-06-26 16:55:51 +0900

    [update] 項目30 動作確認モジュール
    単語数を返すジェネレータ関数.

--- chapter.4/30.return-generator.py
+++ chapter.4/30.return-generator.py
@@ -1,6 +1,9 @@
 """項目30:リストを返さずにジェネレータを返すことだけを考える."""
 
 
+from typing import Iterator
+
+
 def _index_words(text: str) -> list[int]:
     result = []
     if text:
@@ -13,7 +16,19 @@ def _index_words(text: str) -> list[int]:
     return result
 
 
+def _index_word_iter(text: str) -> Iterator[int]:
+    if text:
+        yield 0
+    for index, letter in enumerate(text):
+        if letter == " ":
+            yield index + 1
+
+
 if __name__ == "__main__":
     address = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
     result = _index_words(address)
     print(result[:10])
+
+    it = _index_word_iter(address)
+    print(next(it))
+    print(next(it))
