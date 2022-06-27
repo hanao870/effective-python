f3d11018eeb78b06056e5b81eb0431a339ad769b
hanao870 <hanackholehole@gmail.com>
2022-06-27 20:27:38 +0900

    [add] 項目33 動作確認モジュール
    複数ジェネレータの動作例.

---
+++ chapter.4/33.yield-from.py
@@ -0,0 +1,35 @@
+"""項目33:yield from で複数のジェネレータを作る."""
+from typing import Callable, Iterator
+
+
+def _move(period: int, speed: float) -> Iterator[float]:
+    for _ in range(period):
+        yield speed
+
+
+def _pause(delay: int) -> Iterator[int]:
+    for _ in range(delay):
+        yield 0
+
+
+def _animate() -> Iterator[int | float]:
+    for delta in _move(4, 5.0):
+        yield delta
+    for delta_p in _pause(3):
+        yield delta_p
+    for delta in _move(2, 3.0):
+        yield delta
+
+
+def _render(delta: float) -> None:
+    print(f"Delta: {delta:.1f}")
+    # イメージをスクリーン上で動かす
+
+
+def _run(func: Callable[[], Iterator[int | float]]) -> None:
+    for delta in func():
+        _render(delta)
+
+
+if __name__ == "__main__":
+    _run(_animate)
