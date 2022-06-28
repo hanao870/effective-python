746a6e8b0972f5959bd765ec092d2cb5fba70c75
hanao870 <hanackholehole@gmail.com>
2022-06-28 21:31:54 +0900

    [update] 項目35 動作確認モジュール
    ジェネレータの try/except で例外捕捉.

--- chapter.4/35.generator-throw.py
+++ chapter.4/35.generator-throw.py
@@ -14,8 +14,28 @@ def _my_generator() -> Generator[int, None, None]:
     yield 3
 
 
+def _my_generator_1() -> Generator[int, None, None]:
+    yield 1
+
+    try:
+        yield 2
+    except MyError:
+        print("Got MyError!!!")
+    else:
+        yield 3
+
+    yield 4
+
+
 if __name__ == "__main__":
     it = _my_generator()
     print(next(it))
     print(next(it))
+    # print(it.throw(MyError("test error")))
+    print("-" * 50)
+
+    it = _my_generator_1()
+    print(next(it))
+    print(next(it))
     print(it.throw(MyError("test error")))
+    print("-" * 50)
