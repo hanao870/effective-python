b4076d38d219414745288d8e1d5847d9c35f0dcc
hanao870 <hanackholehole@gmail.com>
2022-06-28 21:44:30 +0900

    [update] 項目35 動作確認モジュール
    ジェネレータの try/except で例外捕捉.
    例外補足でリセットを行う _timer 関数.

--- chapter.4/35.generator-throw.py
+++ chapter.4/35.generator-throw.py
@@ -8,6 +8,12 @@ class MyError(Exception):
     pass
 
 
+class Reset(Exception):
+    """タイマーリセット用例外."""
+
+    pass
+
+
 def _my_generator() -> Generator[int, None, None]:
     yield 1
     yield 2
@@ -27,6 +33,58 @@ def _my_generator_1() -> Generator[int, None, None]:
     yield 4
 
 
+def _timer(period: int) -> Generator[int, None, None]:
+    current = period
+    while current:
+        current -= 1
+        try:
+            yield current
+        except Reset:
+            current = period
+
+
+RESETS = [
+    False,
+    False,
+    False,
+    True,
+    False,
+    True,
+    False,
+    False,
+    False,
+    False,
+    False,
+    False,
+    False,
+    False,
+]
+
+
+def _check_for_reset() -> bool:
+    # 外部イベントをポーリングして待つ
+    return RESETS.pop(0)
+
+
+def _announce(remaining: int) -> None:
+    print(f"{remaining} ticks remaining")
+
+
+def _run() -> None:
+    it = _timer(4)
+
+    while True:
+        try:
+            if _check_for_reset():
+                current = it.throw(Reset())
+            else:
+                current = next(it)
+        except StopIteration:
+            break
+        else:
+            _announce(current)
+
+
 if __name__ == "__main__":
     it = _my_generator()
     print(next(it))
@@ -38,4 +96,7 @@ if __name__ == "__main__":
     print(next(it))
     print(next(it))
     print(it.throw(MyError("test error")))
+    print("-" * 50)
+
+    _run()
     print("-" * 50)
