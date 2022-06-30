e884407a18713f43c54f6bf020fb48c881c8eedf
hanao870 <hanackholehole@gmail.com>
2022-06-30 21:10:32 +0900

    [update] 項目37 動作確認モジュール
    学生集団の科目毎の成績を記録するクラス追加.

--- chapter.5/37.create-class.py
+++ chapter.5/37.create-class.py
@@ -1,4 +1,5 @@
 """項目37:組み込み型の深い入れ子にはせずクラスを作成する."""
+from collections import defaultdict
 
 
 class SimpleGradebook:
@@ -38,6 +39,109 @@ class SimpleGradebook:
         return sum(grades) / len(grades)
 
 
+class BySubjectGradebook:
+    """科目毎に成績のリストを管理するクラス."""
+
+    def __init__(self) -> None:
+        """イニシャライザ."""
+        # 型アノテーションを記述すると既に複雑...
+        # Any で代替可能だが、型が不明...
+        # self._grades: dict[str, Any] = {}
+        self._grades: dict[str, dict[str, list[int]]] = {}
+
+    def add_student(self, name: str) -> None:
+        """生徒を追加する.
+
+        Args:
+            name (str): 生徒名
+        """
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name: str, subject: str, grade: int) -> None:
+        """生徒の科目毎の成績を追加する.
+
+        Args:
+            name (str): 生徒名
+            subject (str): 科目名
+            grade (int): 科目の成績
+        """
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
+
+    def average_grade(self, name: str) -> float:
+        """生徒の全成績の平均点を計算する.
+
+        Args:
+            name (str): 生徒名
+
+        Returns:
+            float: 全教科の平均点
+        """
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grade in by_subject.values():
+            total += sum(grade)
+            count += len(grade)
+
+        return total / count
+
+
+class WeightedGradebook:
+    """科目毎に成績と重みのリストを管理するクラス."""
+
+    def __init__(self) -> None:
+        """イニシャライザ."""
+        # 型アノテーションを記述すると複雑さが顕著...
+        # Any で代替可能だが、型が不明...
+        # self._grades: dict[str, Any] = {}
+        self._grades: dict[str, dict[str, list[tuple[int, float]]]] = {}
+
+    def add_student(self, name: str) -> None:
+        """生徒を追加する.
+
+        Args:
+            name (str): 追加する生徒名
+        """
+        self._grades[name] = defaultdict(list)
+
+    def repot_grade(self, name: str, subject: str, score: int, weight: float) -> None:
+        """生徒の科目毎の成績と重みを追加する.
+
+        Args:
+            name (str): 生徒名
+            subject (str): 科目名
+            score (int): 成績
+            weight (float): 重み
+        """
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append((score, weight))
+
+    def average_grade(self, name: str) -> float:
+        """生徒の全成績の平均点を計算する.
+
+        Args:
+            name (str): 生徒名
+
+        Returns:
+            float: 全成績の平均点
+        """
+        by_subject = self._grades[name]
+        score_sum, score_count = 0.0, 0
+
+        for _, scores in by_subject.items():
+            subject_avg, total_weight = 0.0, 0.0
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+
+            score_sum += subject_avg / total_weight
+            score_count += 1
+
+        return score_sum / score_count
+
+
 if __name__ == "__main__":
     book = SimpleGradebook()
     book.add_student("Isaac Newton")
@@ -46,3 +150,20 @@ if __name__ == "__main__":
     book.report_grade("Isaac Newton", 85)
 
     print(book.average_grade("Isaac Newton"))
+
+    book1 = BySubjectGradebook()
+    book1.add_student("Albert Einstein")
+    book1.report_grade("Albert Einstein", "Math", 75)
+    book1.report_grade("Albert Einstein", "Math", 65)
+    book1.report_grade("Albert Einstein", "Gym", 90)
+    book1.report_grade("Albert Einstein", "Gym", 95)
+    print(book1.average_grade("Albert Einstein"))
+
+    book2 = WeightedGradebook()
+    book2.add_student("Albert Einstein")
+    book2.repot_grade("Albert Einstein", "Math", 75, 0.05)
+    book2.repot_grade("Albert Einstein", "Math", 65, 0.15)
+    book2.repot_grade("Albert Einstein", "Math", 70, 0.80)
+    book2.repot_grade("Albert Einstein", "Gym", 100, 0.40)
+    book2.repot_grade("Albert Einstein", "Gym", 85, 0.60)
+    print(book2.average_grade("Albert Einstein"))
