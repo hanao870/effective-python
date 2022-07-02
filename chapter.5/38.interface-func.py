6abcf852ba6b26c46a6f45e29fbb0e10fe7b542b
N.Miki <hanackholehole@gmail.com>
2022-07-02 20:16:35 +0900

    [update] 項目38 動作確認モジュール
    defaultdict の第1引数に関数を指定.
    指定した関数はキーが見つからなかったら呼ばれる.

--- chapter.5/38.interface-func.py
+++ chapter.5/38.interface-func.py
@@ -1,7 +1,26 @@
 """項目38:単純なインターフェイスにはクラスの代わりに関数を使う."""
+from collections import defaultdict
+
+
+def _log_missing() -> int:
+    print("Key added")
+    return 0
 
 
 if __name__ == "__main__":
     names = ["Socrates", "Archimedes", "Plato", "Aristotle"]
     names.sort(key=len)
     print(names)
+
+    current = {"green": 12, "blue": 3}
+    increments = [
+        ("red", 5),
+        ("blue", 17),
+        ("orange", 9),
+    ]
+    # キーが見つからなかったら呼ばれる関数を第1引数に指定
+    result = defaultdict(_log_missing, current)
+    print(f"Brfore: {dict(result)}")
+    for key, amount in increments:
+        result[key] += amount
+    print("After: ", dict(result))
