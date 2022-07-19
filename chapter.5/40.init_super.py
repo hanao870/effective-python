68f54a9a168df1e4ebb34d7b89a239358f884c1e
N.Miki <hanackholehole@gmail.com>
2022-07-20 08:30:25 +0900

    [update] 項目39 動作確認モジュール
    ひし形継承と super().__init__ による動作確認.
    呼び出し順は MRO で確認する.

--- chapter.5/40.init_super.py
+++ chapter.5/40.init_super.py
@@ -87,6 +87,44 @@ class ThisWay(TimesSeven, PlusNine):
         PlusNine.__init__(self, value)
 
 
+class TimesSevenCorrect(MyBaseClass):
+    """ひし形継承動作確認クラス.
+
+    基底クラスの初期化は super を使用.
+    """
+
+    def __init__(self, value: int) -> None:
+        """イニシャライザ."""
+        print("Call TimesSevenCorrect __init__")
+        super().__init__(value)
+        self.value *= 7
+
+
+class PlusNineCorrect(MyBaseClass):
+    """ひし形継承動作確認クラス.
+
+    基底クラスの初期化は super を使用.
+    """
+
+    def __init__(self, value: int) -> None:
+        """イニシャライザ."""
+        print("Call PlusNineCorrect __init__")
+        super().__init__(value)
+        self.value += 9
+
+
+class GoodWay(TimesSevenCorrect, PlusNineCorrect):
+    """ひし形継承派生クラス.
+
+    問題なく動作する.
+    """
+
+    def __init__(self, value: int) -> None:
+        """イニシャライザ."""
+        print("Call GoodWay __init__")
+        super().__init__(value)
+
+
 if __name__ == "__main__":
     foo = OneWay(5)
     print(f"First ordering is (5 * 2) + 5 = {foo.value}")
@@ -99,3 +137,10 @@ if __name__ == "__main__":
     # hoge.value は 14 となる.
     # MyBaseClass.__init__ の2回呼び出しの為、値がリセットされる.
     print(f"Should be (5 * 7) + 9 = 44 but is {hoge.value}")
+
+    fuga = GoodWay(5)
+    print(f"Should be 7 * (5 + 9) = 98 and is {fuga.value}")
+
+    # クラス呼び出し順 MRO(Method Resolution Order) の確認.
+    mro_str = "\n".join(repr(cls) for cls in GoodWay.mro())
+    print(mro_str)
